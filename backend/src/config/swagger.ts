2a1ed673d960000829811b8fc2e31d10cf1ffe8e
Dobby <dobby@example.xyz>
2026-02-25 17:37:07 +0400

    fix: lab4 Small bug-fix of SwaggeUI. Now descriptions of the tags appears

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -28,7 +28,7 @@ const options: Options = {
   },
   // Path to the API docs
   // Note: update these paths to match your TypeScript source files
-  apis: ['src/controller/*.ts', 'src/database/model/*.ts', 'src/swagger.ts'],
+  apis: ['src/controller/*.ts', 'src/database/model/*.ts', 'src/config/swagger.ts'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
